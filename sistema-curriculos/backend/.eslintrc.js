f6696f861799291770488478b8b9f77145dde486
Victor Zion França <vzion435@gmail.com>
2024-11-23 22:42:06 -0300

    alterações

---
+++ sistema-curriculos/backend/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+      node: true, // Adicione isso para habilitar o ambiente Node.js
+      es2021: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 12,
+    },
+    rules: {
+      // Suas regras personalizadas aqui
+    },
+  };
+  
